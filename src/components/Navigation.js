415740253ad274ee98a54fe57ca8756b34699a91
krzosdominik <krzosdominik@gmail.com>
2020-03-31 01:12:56 +0200

    add react scroll

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import { Link } from "react-scroll";
+
+const options = {
+    spy: true,
+    smooth: true,
+    duration: 500,
+    delay: 300
+};
 
 const Navigation = () => {
     return (
         <nav className="navigation">
             <ul className="navigation__list">
-                <li className="navigation__item">Strona główna</li>
-                <li className="navigation__item">Projekty</li>
-                <li className="navigation__item">O mnie</li>
-                <li className="navigation__item">Kontakt</li>
+                <li className="navigation__item">
+                    <Link to="home" {...options}>
+                        Strona główna
+                    </Link>
+                </li>
+                <li className="navigation__item">
+                    <Link to="projects" {...options}>
+                        Projekty
+                    </Link>
+                </li>
+                <li className="navigation__item">
+                    <Link to="about" {...options}>
+                        O mnie
+                    </Link>
+                </li>
+                <li className="navigation__item">
+                    <Link to="contact" {...options}>
+                        Kontakt
+                    </Link>
+                </li>
             </ul>
         </nav>
     )

bbe63fbf1517a8e36c945bd3e459acd12066deb8
krzosdominik <krzosdominik@gmail.com>
2020-04-01 00:56:13 +0200

    add hamburger and fix some layout errors

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import Hamburger from './Hamburger';
 import Navigation from "./Navigation";
+import overflowBlock from '../methods/overflowBlock';
 
 const Header = () => {
+    const [isActive, setIsActive] = useState(false);
+
+    const handleHamburgerClick = () => setIsActive(!isActive);
+    const handleNavigationClick = () => setIsActive(false);
+
+    overflowBlock(isActive);
+
     return (
-        <header className='header'>
-            <Link to='/' className='header__brand'>
-                Krzos Dominik <span>Portfolio</span>
-            </Link>
-            <Navigation />
-        </header>
+        <>
+            <header className='header'>
+                <Link to='/' className='header__brand'>
+                    Krzos Dominik <span>Portfolio</span>
+                </Link>
+                <Hamburger onClick={handleHamburgerClick} isActive={isActive} />
+                <Navigation
+                    onClick={handleNavigationClick}
+                    classes={isActive
+                        ? "navigation navigation--active"
+                        : "navigation"}
+                />
+            </header>
+        </>
     )
 };
 

1ccbcaf1327249d25438f9b2e17f7d312398f9e3
krzosdominik <krzosdominik@gmail.com>
2020-03-31 11:15:20 +0200

    add homepage section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
        <Switch>
        <Route exact path="/">
           <Header />
+          <Main />
         </Route>
        </Switch>
     </BrowserRouter>
---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <section id='home' className='home'>
+            <h1 className='home__header'>Krzos <span>Dominik</span></h1>
+            <p className="home__paragraph">Junior <span>Frontend</span> Developer</p>
+        </section>
+    )
+};
+
+export default Main;

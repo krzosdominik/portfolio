4bea458a9d855c8a7f81f77c17c2f225d712c382
krzosdominik <krzosdominik@gmail.com>
2020-04-01 21:47:47 +0200

    add footer and small scss changes

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p className="footer__copyright">
+            © 2020 <a href="https://www.linkedin.com/in/krzosdominik/">Krzos Dominik</a>  | All Rights Reserved
+            </p>
+        </footer>
+    )
+}
+
+export default Footer;
